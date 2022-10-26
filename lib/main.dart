1aec373a8f27665421ce3bc347dfc01d5e289a01
tigerBR0 <khurbanovrakhmiddin@gmail.com>
2022-10-27 00:32:54 +0500

    added about view

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,20 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:intex_mobile_app/core/constants/theme.dart';
 import 'package:intex_mobile_app/features/screens/main_page/view/main_page.dart';
 
-void main() {
-  runApp(const MyApp());
+void main()async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+
+
+  runApp( EasyLocalization(
+      supportedLocales: [Locale('en', 'US'), Locale('ru', 'RU'),
+        Locale('uz', 'UZ')],
+      path: 'assets/translations', // <-- change the path of the translation files
+      fallbackLocale: Locale('ru', 'RU'),
+      child: MyApp()
+  ));
 }
 
 class MyApp extends StatelessWidget {
@@ -10,9 +22,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return  MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: MainPage(),
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      locale: context.locale,
+      theme: light,
+      home: const MainPage(),
     );
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -27,4 +27,11 @@ void main() {
     expect(find.text('0'), findsNothing);
     expect(find.text('1'), findsOneWidget);
   });
+
+  test('Network Check', (){
+
+    // expect(actual, matcher);
+
+
+  });
 }
