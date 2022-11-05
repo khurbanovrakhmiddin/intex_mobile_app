106a7486d3b452d9e22f82795714c84d4489e16a
tigerBR0 <khurbanovrakhmiddin@gmail.com>
2022-11-05 21:45:34 +0500

    7 step added DetailPage
    Redesign

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:intex_mobile_app/core/constants/theme.dart';
 import 'package:intex_mobile_app/features/screens/main_page/view/main_page.dart';
 
