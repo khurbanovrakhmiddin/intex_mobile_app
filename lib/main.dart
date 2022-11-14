140f4427a80bac0255c3bf969f23d07179fc43ec
tigerBR0 <khurbanovrakhmiddin@gmail.com>
2022-11-14 23:51:00 +0500

    8 step added DetailPage
    Redesign added Category Page

--- lib/main.dart
+++ lib/main.dart
@@ -2,22 +2,19 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:intex_mobile_app/core/constants/theme.dart';
-import 'package:intex_mobile_app/core/repository/catalog_repository.dart';
 import 'package:intex_mobile_app/core/service/db_service.dart';
 import 'package:intex_mobile_app/core/service/observ_service.dart';
-import 'package:intex_mobile_app/features/screens/conect_page/bloc/connect_bloc.dart';
-import 'package:intex_mobile_app/features/screens/main_page/bloc/main_bloc.dart';
-import 'package:intex_mobile_app/features/screens/main_page/view/main_page.dart';
+import 'package:intex_mobile_app/features/screens/conect_page/view/connect_check/checker/check_state.dart';
+import 'features/app.dart';
+import 'features/screens/conect_page/view/connect_check/checker/check_servicde.dart';
 
-import 'core/service/connect_check/check_servicde.dart';
-import 'core/service/connect_check/check_state.dart';
+
 
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
   await DBService.init();
-
   try {
   } catch (e) {
     print('error: $e');
@@ -32,45 +29,45 @@ void main() async {
       ],
       path: 'assets/translations',
       fallbackLocale: const Locale('ru', 'RU'),
-      child: const MyApp(
-      )));
+      child:  MyApp()));
 }
 
 class MyApp extends StatelessWidget {
 
-
-  const MyApp(
-      {super.key});
+   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     print('MyApp');
 
+      return  MaterialApp(
+        debugShowCheckedModeBanner: false,
+        localizationsDelegates: context.localizationDelegates,
+        supportedLocales: context.supportedLocales,
+        locale: context.locale,
+        theme: light,
+        builder: (context, child) {
+            return MediaQuery(
+              data: MediaQuery.of(context).copyWith(
+                textScaleFactor: 1,
+                boldText: false,
+              ),
+              child:child!
+          );
+        },
+        home:   App(),
 
-   return MaterialApp(
-     debugShowCheckedModeBanner: false,
-     localizationsDelegates: context.localizationDelegates,
-     supportedLocales: context.supportedLocales,
-     locale: context.locale,
-     theme: light,
-     builder: (context, child) {
+
+
+      );
+    }
 
 
 
-       return MediaQuery(
-         data: MediaQuery.of(context).copyWith(
-           textScaleFactor: 1,
-           boldText: false,
-         ),
-         child:child!
-       );
-     },
-
-     home: const MainPage(),
-   );
 
 
 
 
   }
-}
+
+
