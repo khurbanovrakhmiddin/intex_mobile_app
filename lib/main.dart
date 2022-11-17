6c75f66f3e30da2e88c44ade83f5bbb2cec31b50
tigerBR0 <khurbanovrakhmiddin@gmail.com>
2022-11-17 19:41:57 +0500

    9 step added DetailPage
    Add CartBloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,29 @@
+import 'dart:convert';
+
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
 import 'package:intex_mobile_app/core/constants/theme.dart';
 import 'package:intex_mobile_app/core/repository/product_by_status_repository.dart';
 import 'package:intex_mobile_app/core/service/db_service.dart';
 import 'package:intex_mobile_app/core/service/observ_service.dart';
+import 'package:intex_mobile_app/features/screens/main_page/view/main_page.dart';
+import 'core/models/Category.dart';
 import 'core/repository/catalog_repository.dart';
 import 'core/service/go_service.dart';
+import 'features/app.dart';
+import 'package:intex_mobile_app/core/repository/cart_repository.dart';
+import 'package:intex_mobile_app/core/utils/product_category.dart';
+import 'package:intex_mobile_app/features/screens/cart_page/bloc/products_bloc.dart';
+import 'package:intex_mobile_app/features/screens/cart_page/cart_bloc/cart_bloc.dart';
+
 import 'features/screens/cart_page/first_bloc/first_product_bloc.dart';
 import 'features/screens/cart_page/second_bloc/second_product_bloc.dart';
 import 'features/screens/cart_page/third_bloc/third_product_bloc.dart';
+import 'features/screens/conect_page/view/connect_check/checker/check_servicde.dart';
+import 'features/screens/conect_page/view/connect_check/checker/check_state.dart';
+import 'features/screens/detail_page/page/detail_page.dart';
 import 'features/screens/main_page/bloc/main_bloc.dart';
 
 
@@ -38,8 +52,99 @@ void main() async {
 
 class MyApp extends StatelessWidget {
 
-   const MyApp({super.key});
+    MyApp({super.key});
+      final router = GoRouter(
+     redirect: (context, state) async{
+       ConnectionStatus status =await CheckInternetConnection.connect();
+       var connect = status.name ==
+           ConnectionStatus
+               .online.name;
 
+       if (!connect) {
+       //  local = state.location.isEmpty?'/':state.location;
+         return '/connect';
+       }
+
+       if (connect) {
+         return '/';
+       }
+
+       return null;
+     },
+
+     refreshListenable: ConnectionStatusValueNotifier(),
+
+
+     initialLocation: '/',
+     routes: [
+       GoRoute(
+           path: '/',
+           name: 'Main',
+           pageBuilder: (context, state) {
+             return CustomTransitionPage(
+               key: state.pageKey,
+               child: const MainPage(),
+               transitionsBuilder: (context, animation, secondaryAnimation, child) {
+                 return FadeTransition(
+                   opacity:
+                   CurveTween(curve: Curves.easeInQuad).animate(animation),
+                   child: child,
+                 );
+               },
+             );
+           }),
+       GoRoute(
+           path: '/connect',
+           name: 'NoConnect',
+           pageBuilder: (context, state) {
+
+             return CustomTransitionPage(
+               key: state.pageKey,
+               child: const OffLinePage(),
+               transitionsBuilder: (context, animation, secondaryAnimation, child) {
+                 return FadeTransition(
+                   opacity:
+                   CurveTween(curve: Curves.easeInQuad).animate(animation),
+                   child: child,
+                 );
+               },
+             );
+           }
+       ),
+       GoRoute(
+         path: '/detail/:category',
+         name: 'Detail',
+         pageBuilder: (context, state) {
+
+
+           Category category = Category.fromJson(jsonDecode(state
+               .params['category']!));
+
+           return CustomTransitionPage(
+             key: state.pageKey,
+             child:  DetailPage(category: category),
+             transitionsBuilder: (context, animation, secondaryAnimation, child) {
+               return FadeTransition(
+                 opacity:
+                 CurveTween(curve: Curves.easeInQuad).animate(animation),
+                 child: child,
+               );
+             },
+           );
+
+         },
+       ),
+
+     ],
+     errorPageBuilder: (context, state) => MaterialPage(
+       key: state.pageKey,
+       child: Scaffold(
+         body: Center(
+           child: Text(state.error.toString()),
+         ),
+       ),
+     ),
+   );
   @override
   Widget build(BuildContext context) {
     print('MyApp');
@@ -59,24 +164,27 @@ class MyApp extends StatelessWidget {
             create: (context) => ThirdProductBloc(
                 ProductByStatusRepository())),
 
-      ], child: MaterialApp.router(
-        debugShowCheckedModeBanner: false,
-        localizationsDelegates: context.localizationDelegates,
-        supportedLocales: context.supportedLocales,
-        locale: context.locale,
-        theme: light,
-        builder: (context, child) {
-            return MediaQuery(
-              data: MediaQuery.of(context).copyWith(
-                textScaleFactor: 1,
-                boldText: false,
-              ),
-              child:child!
-          );
-        },
-       routerConfig: GoService.router,
+      ], child:  MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+    localizationsDelegates: context.localizationDelegates,
+    supportedLocales: context.supportedLocales,
+    locale: context.locale,
+    theme: light,
+    builder: (context, child) {
+      return ScrollConfiguration(
+        behavior: MyBehavior(),
+        child: MediaQuery(
+            data: MediaQuery.of(context).copyWith(
+              textScaleFactor: 1,
+              boldText: false,
+            ),
+            child:child!
+        ),
+      );
 
-      ));
+    },
+    routerConfig: router,
+    ));
     }
 
 
@@ -87,3 +195,10 @@ class MyApp extends StatelessWidget {
   }
 
 
+class MyBehavior extends ScrollBehavior {
+  @override
+  Widget buildOverscrollIndicator(
+      BuildContext context, Widget child, ScrollableDetails details) {
+    return child;
+  }
+}
